b5c7027438874d676e0a2f74ce4b749e605f355f
RoDoerIng <ronnydoering.ing@gmail.com>
2019-11-17 14:18:57 +0100

    Projektdateien hinzufügen.

---
+++ BusinessTrips/Model/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessTrips.Model
+{
+    public class Address
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        [Required]
+        public Name Name { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string StreetNumber { get; set; }
+
+        [Required]
+        public string PostCode { get; set; }
+
+        [Required]
+        public string City { get; set; }
+    }
+}
---
+++ BusinessTrips/Model/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessTrips.Model
+{
+    public class Name
+    {
+        [Key]
+        public int NameId { get; set; }
+
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+    }
+}
