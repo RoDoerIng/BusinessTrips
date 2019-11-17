2570845f86565bb9c78f5a075cf00840807a8ef8
RoDoerIng <ronnydoering.ing@gmail.com>
2019-11-17 14:39:35 +0100

    Add trip model

---
+++ BusinessTrips/Model/Trip.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessTrips.Model
+{
+    public class Trip
+    {
+        [Key]
+        public int TripId { get; set; }
+
+        private DateTime _date = DateTime.MinValue;
+
+        [Required]
+        public DateTime Date
+        {
+
+            get => _date;
+            set => _date = value;
+        }
+
+        [Range(0,double.MaxValue)]
+        public decimal Distance { get; set; }
+
+        [Required]
+        public Address StartAddress { get; set; }
+
+        [Required]
+        public Address DestinationAddress { get; set; }
+    }
+}
