146d10cf814d5b440498918eb2ffa74ad23c9bcc
RoDoerIng <ronnydoering.ing@gmail.com>
2019-11-17 14:33:31 +0100

    Add person model

---
+++ BusinessTrips/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessTrips.Model
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+
+        [Required]
+        public Name Name { get; set; }
+
+        [Required]
+        public Address Address { get; set; }
+    }
+}
